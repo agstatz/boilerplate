ddbc519df711952e20fa2760959907650f1a3b32
Ashton <90576442+agstatz@users.noreply.github.com>
2022-01-31 20:01:27 -0500

    Added node/express to backend application folder

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+/**
+ * index.js
+ * entry point of the API Server
+ * 
+ * @author Ashton Statz
+ */
+
+ const express = require('express');
+
+// Create an Express application
+const app = express();
