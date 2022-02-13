9d9ffdb3508c3e718c01e1df4a62de9d4f3014d1
Dawson Smith <dawsonsmith64@yahoo.com>
2022-02-12 19:30:54 -0500

    fixbug

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from "./Home.tsx";
-export { default as PageNotFound } from "./PageNotFound.tsx";export { default as PageNotFound } from "./PageNotFound.tsx";
+export { default as PageNotFound } from "./PageNotFound.tsx";
 export { default as LoginForm } from "./LoginForm.js";
 export { default as RegisterForm } from "./RegisterForm.js";
