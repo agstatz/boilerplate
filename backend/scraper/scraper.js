36fb279791ae7eb4b31c38c4e25cdf069b22643c
Arjan Mobin <arjanmobin@gmail.com>
2022-02-18 02:24:22 -0500

    gathering all current meals

--- backend/scraper/scraper.js
+++ backend/scraper/scraper.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const MongoClient = require('mongodb').MongoClient;
 const puppeteer = require('puppeteer');
+const selectors = require('./selectors');
+
 
 
 const url = "mongodb+srv://admin:1234@cluster0.ggera.mongodb.net/boilerplate?retryWrites=true&w=majority"; 
@@ -15,26 +17,48 @@ MongoClient.connect(url, async (err, dbt) => {
     const page = await browser.newPage();
     await page.goto(DINING_PAGE_URL);
     let html = await page.content();
-    const $ = cheerio.load(html);
-    const dining_menu = $('#app > div > div > div.MuiGrid-root.menus__home-content--container.MuiGrid-container > div > div > div:nth-child(1) > div > ul > a:nth-child(1) > li > div.MuiListItemText-root.MuiListItemText-multiline > span');
-    console.log(dining_menu.contents().text())
+    let page_details = await page.evaluate(() => {return document});
+    let $ = cheerio.load(html);
 
-    // console.log(html)
-    // let q = await page.evaluate(() => {return document.querySelector('#app > div > div > div.MuiGrid-root.menus__home-content--container.MuiGrid-container > div > div > div:nth-child(1) > div > ul > a:nth-child(1) > li > div.MuiListItemText-root.MuiListItemText-multiline > span').innerText});
-    // console.log(q);
+    for (const [dining_court, selector] of Object.entries(selectors.dining_court_links)) {
+        const menu_link = page_details.location.origin +  $(selector).attr('href');
+        console.log(menu_link)
+
+        await get_stations(browser, menu_link)
+
+        // let stations = $('#main > div.meal')
+        // console.log(stations)
+    }
+
+
     await browser.close();
-    
-    console.log("done")
-
-
-
-    
-    // let page = await get_page(DINING_PAGE_URL);
-    // console.log(page);
-    // const $ =  cheerio.load(page);
-
-
-    // let earhart = $.html();
-    // console.log(earhart)
     dbt.close();
 });
+
+
+async function get_stations(browser, menu_link) {
+    const page = await browser.newPage();
+    await page.goto(menu_link);
+    const $ = cheerio.load(await page.content());
+    let stations_html = $('.station-container')
+
+    let stations = {}
+    
+    for (const station of stations_html) {
+        let station_name = $(station).find('.station-name').text()
+        console.log('STATION: ', station_name)
+
+        for (const item of $(station).find('.station-item')) {
+            let item_link  = $(item).attr('href')
+            let item_name = $(item).find('.station-item-text').text()
+            console.log(item_name, item_link)
+        }
+        console.log('\n')
+
+        // console.log(station.children[0].children[0].data)
+    }
+
+    
+
+
+}
