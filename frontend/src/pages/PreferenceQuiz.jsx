b9c6b97c6d0ae3563fc743da092b842bb4681705
Ashton <90576442+agstatz@users.noreply.github.com>
2022-02-22 19:37:18 -0500

    Added Quiz Question

--- frontend/src/pages/PreferenceQuiz.jsx
+++ frontend/src/pages/PreferenceQuiz.jsx
@@ -23,11 +23,17 @@ import { store } from "../store/store.js"
 
 function PreferenceQuiz() {
 
-    const [nutritionScore, setNutritionScore] = useState(0);        // how importatnt nutrition is to user
+    const [meatScore, setMeatScore] = useState(0);                  // meat dietary restrictions
+    const [dairyScore, setDairyScore] = useState(0);                // dairy dietary restrictions
+    const [glutenScore, setGlutenScore] = useState(0);              // gluten dietary restrictions
+    const [nutScore, setNutScore] = useState(0);                    // nut dietary restrictions
+    const [nutritionScore, setNutritionScore] = useState(0);        // nutrition dietary score
+
+
     const [questionNumber, setQuestionNumber] = useState(0);        // stores the q number user is on
 
     const username = store.getState().app.username;
-    
+
     const incrementQuestion = () => {
         const newNum = questionNumber + 1;
         setQuestionNumber(newNum);
@@ -74,6 +80,11 @@ function PreferenceQuiz() {
                             <Form.Check 
                                 type='radio'
                                 label='Pescatarian'
+                                name="veg_status"
+                            />
+                            <Form.Check 
+                                type='radio'
+                                label='None of these apply'
                                 name="veg_status"
                             />
                         </Stack>
@@ -132,7 +143,7 @@ function PreferenceQuiz() {
                             <Button className="mx-2" onClick={incrementQuestion}>Next <i className="bi bi-chevron-right"></i></Button>
                         </Container>
                     </div>
-                )
+                );
             case 4:
                 return (
                     <div className="p-3 my-4 mx-4 bg-light border rounded">
@@ -162,9 +173,40 @@ function PreferenceQuiz() {
                 return (
                     <div className="p-3 my-4 mx-4 bg-light border rounded">
                         <Stack gap={2}>
-                            <h1>Question 4 </h1>
+                            <h1>Question 5 </h1>
+                            <p>Do you observe any of the following dietary religious laws?</p>
+                            <Stack className="p-1 d-flex justify-content-center">
+                                <Form.Check 
+                                    type='radio'
+                                    label='Halal'
+                                    name="law_status"
+                                />
+                                <Form.Check 
+                                    type='radio'
+                                    label='Kosher'
+                                    name="law_status"
+                                />
+                                <Form.Check 
+                                    type='radio'
+                                    label='None'
+                                    name="law_status"
+                                />
+                            </Stack>
+                            <br />
+                        </Stack>
+                        <Container className="d-flex justify-content-center">
+                            <Button className="mx-2" onClick={decrementQuestion}><i className="bi bi-chevron-left"></i> Back</Button>
+                            <Button className="mx-2" onClick={incrementQuestion}>Next <i className="bi bi-chevron-right"></i></Button>
+                        </Container>
+                    </div>
+                );
+            case 6:
+                return (
+                    <div className="p-3 my-4 mx-4 bg-light border rounded">
+                        <Stack gap={2}>
+                            <h1>Question 6 </h1>
                             <p>How important is nutritional value of the food you eat to you?
-                                 Rate 5 for very important, 0 for not important at all</p>
+                                 Rate 5 for very important, 0 for not important at all.</p>
                             <Container className="p-1 d-flex justify-content-center">
                             <RangeSlider
                                 value={nutritionScore}
@@ -177,11 +219,11 @@ function PreferenceQuiz() {
                         </Stack>
                         <Container className="d-flex justify-content-center">
                             <Button className="mx-2" onClick={decrementQuestion}><i className="bi bi-chevron-left"></i> Back</Button>
-                            <Button className="mx-2" onClick={incrementQuestion}>Next <i className="bi bi-chevron-right"></i></Button>
+                            <Button className="mx-2" onClick={incrementQuestion}>Submit Preferences <i className="bi bi-chevron-right"></i></Button>
                         </Container>
                     </div>
                 );
-            case 6:
+            case 7:
                 return (
                     <div className="p-3 my-4 mx-4 bg-light border rounded">
                         <Stack gap={2}>
