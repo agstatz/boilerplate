daf1088495d205c0cba749cb1b1109119fed1c96
Colin Cross <crosstco@gmail.com>
2022-02-24 13:52:44 -0500

    Rename connectDB callback function to be more specific.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
+// Database URI. Store in environment variable later.
 const db = 'mongodb+srv://admin:1234@cluster0.ggera.mongodb.net/boilerplate?retryWrites=true&w=majority'
 
-const connectDB = async (callback) => {
+const connectDB = async (onConnect) => {
     try {
         await mongoose.connect(db);
         console.log('MongoDB Connected');
-        callback();
+        onConnect();
     } catch (err) {
         console.error(err.message);
         process.exit(1);
