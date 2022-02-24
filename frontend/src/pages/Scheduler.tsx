0b54c469a51fc76aa9b4deac4a3834f2f5e6daae
Arjan Mobin <arjanmobin@gmail.com>
2022-02-24 15:49:30 -0500

    form for each day of the week

--- frontend/src/pages/Scheduler.tsx
+++ frontend/src/pages/Scheduler.tsx
@@ -14,8 +14,40 @@ class Scheduler extends React.Component {
     name: "",
     description: "",
   };
+
+  
+
   render() {
-    return (
+    function form_column(day:string) {
+      return (
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>
+          <h3>Day:</h3>
+        </Form.Label>
+        <Col sm={4}>
+          <Form.Label column sm={2}>
+            <h2>{day}</h2>
+          </Form.Label>
+        </Col>
+        <Form.Label column sm={2}>
+          <h3>Food:</h3>
+        </Form.Label>
+        <Col sm={4}>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Select a Food Item
+            </Dropdown.Toggle>
+  
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Form.Group>
+      )
+    }
+    
+    return [
       <Container style={{ paddingTop: "15vh", paddingBottom: "15vh" }}>
         <h1>Schedule</h1>
         <div className="p-3 my-4 mx-4 bg-light border rounded">
@@ -31,36 +63,12 @@ class Scheduler extends React.Component {
                   <h3>Meal Schedule Name:</h3>
                 </Form.Label>
                 <Col sm={4}>
-                  <Form.Control type="text" placeholder="healthy"/>
-                </Col>
-                
-              </Form.Group>
-              <Form.Group
-                as={Row}
-                className="mb-3"
-                controlId="formHorizontalEmail"
-              >
-                <Form.Label column sm={2}>
-                  <h3>Date:</h3>
-                </Form.Label>
-                <Col sm={4}>
-                  <Form.Control type="date" placeholder="Email" />
-                </Col>
-                <Form.Label column sm={2}>
-                  <h3>Food:</h3>
-                </Form.Label>
-                <Col sm={4}>
-                  <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                      Select a Food Item
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
+                  <Form.Control type="text" placeholder="healthy" />
                 </Col>
               </Form.Group>
+              {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].map((day, i) => {
+                return form_column(day);
+              })}
             </Form>
           </div>
         </div>
@@ -68,8 +76,8 @@ class Scheduler extends React.Component {
         <Button className="mx-auto btn btn-primary btn-sm" href="/">
           Home
         </Button>
-      </Container>
-    );
+      </Container>,
+    ];
   }
 }
 
